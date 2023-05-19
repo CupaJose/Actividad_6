d5de9afa48f029ba738b200e056e0e384a6b7671
Cupa José <cupa.sepulveda.jc@gmail.com>
2023-05-19 03:39:59 -0600

    Solución problemas con los nombres

--- Pedidos.php
+++ Pedidos.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href=".\concierto_logo.jpg" type="image/jpg">
-    <title>Document</title>
+    <title>Orden</title>
 </head>
 <body>
     <?php
